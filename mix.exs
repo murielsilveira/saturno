287780d5184bcc181d4e1c162cb2510a1c4c9f44
Muriel Silveira <murielkong@gmail.com>
2020-04-02 23:55:55 +0200

    chore: upgrade to elixir 1.10

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Saturno.Mixfile do
     [
       app: :saturno,
       version: "0.0.1",
-      elixir: "~> 1.4",
+      elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
